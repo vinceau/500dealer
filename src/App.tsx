7317dac1e283c67b89ec1c3701666a1d15460d46
Vince Au <vinceau09@gmail.com>
2019-08-18 22:45:11 +1000

    Switch to using hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, RouteComponentProps, Redirect } from "react-router-dom";
+import { HashRouter as Router, Route, RouteComponentProps, Redirect } from "react-router-dom";
 import { GameForm } from "./components/GameForm";
 
 function BasicExample() {
