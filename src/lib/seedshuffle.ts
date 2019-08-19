432bed4e62fb9b2ed33bad9e6829d95d76f92cde
Vince Au <vinceau09@gmail.com>
2019-08-19 16:56:28 +1000

    seedshuffle: use let instead of var

--- src/lib/seedshuffle.ts
+++ src/lib/seedshuffle.ts
@@ -3,9 +3,9 @@ import seedrandom from "seedrandom";
 // Shuffles an array in place using a given seed
 export const seedshuffle = (array: Array<any>, seed: string) => {
     const rng = seedrandom(seed);
-    var i = 0
-        , j = 0
-        , temp = null
+    let i = 0;
+    let j = 0;
+    let temp = null;
 
     for (i = array.length - 1; i > 0; i -= 1) {
         j = Math.floor(rng() * (i + 1))
